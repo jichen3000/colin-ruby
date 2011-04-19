cb221299faf3a5a2cf3010e9fc2c45902eddcb5a
Colin J <jichen3000@gmail.com>
2011-04-19 06:05:28 -0400

    heroku with postgres first

--- test/sequel/postgres_test.rb
+++ test/sequel/postgres_test.rb
@@ -1,2 +1,11 @@
 require 'sequel'
+require 'pg'
+#db=Sequel.connect('postgres://colin@localhost/colindb')
 db=Sequel.connect('postgres://colin@72.46.131.199/colindb')
+db1=Sequel.postgres('colindb', :user=>'colin', :password=>nil,
+	:host=>'72.46.131.199', :port=>5432,
+	:max_connections=>10)
+puts db1[:pg_am].count
+puts db[:pg_am].count
+
+puts "ok"
