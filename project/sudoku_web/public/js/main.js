d061642557a6f06eb760ce1bc712f4a19b6c0048
Colin Ji <jichen3000@gmail.com>
2012-05-24 10:49:58 +0800

    need change dialog

--- project/sudoku_web/public/js/main.js
+++ project/sudoku_web/public/js/main.js
@@ -16,9 +16,10 @@ $(function() {
   var updateElement = null;
 
   $('#sudoku-99').find('.cell').click(function(e){
-    $('#dialog').css('display','');
     var position = { top: e.pageY+5, left: e.pageX+5 }
-    $('#dialog').offset(position);
+    $('#choose-value-dialog').css('display','');
+    $('#choose-value-dialog').modal;
+    $('#choose-value-dialog').offset(position);
     p(e.pageX+":"+e.pageY);
     updateElement = this;
   });
@@ -26,18 +27,20 @@ $(function() {
     alert('123');
   });
   
-  $('#dialog').find('.cell').click(function(e){
+  $('#choose-value-dialog').find('.cell').click(function(e){
     p(this.innerText);
     $(updateElement).addClass(fixClass).text(this.innerText);
     $('#close').click();
+    refreshFixedPointsCount();
   });
   
   $('#close').click(function(){
-    $('#dialog').css('display','none');
+    $('#choose-value-dialog').css('display','none');
   });
   $('#null').click(function(){
     clearCell(updateElement);
     $('#close').click();
+    refreshFixedPointsCount();
   });
   function clearCell(current){
     $(current).text('').removeClass(answeredClass).removeClass(fixClass);
@@ -50,7 +53,12 @@ $(function() {
     return fixedPoints;    
   }
   function validateFixedPoints(){
-    return ($('.'+fixClass).length > 16);
+    var pointsCount = $('.'+fixClass).length+$('.'+answeredClass).length;
+    return (pointsCount >= 17 && pointsCount < 81);
+  }
+  function refreshFixedPointsCount(){
+    var pointsCount = $('.'+fixClass).length+$('.'+answeredClass).length;
+    $(".values-count").text("values count: "+pointsCount);
   }
   function displayPoints(points, needClass){
     for (var key in points){
@@ -58,9 +66,8 @@ $(function() {
     }
   }
   $('button#compute').click(function(){
-    if (!validateFixedPoints()){
-      
-      p("no fixed values!");
+    if (!validateFixedPoints()){ 
+      p("No fixed values!");
       return; 
     }
     
@@ -69,6 +76,7 @@ $(function() {
       displayPoints(JSON.parse(result), answeredClass);
       //p('get success!'+result);
       p('Successful!');
+      refreshFixedPointsCount();
     });
   });
   $('button#clear').click(function(){
@@ -78,10 +86,12 @@ $(function() {
     $('.'+fixClass).each(function(){
       clearCell(this);
     });
+    refreshFixedPointsCount();
+    p("Cleared all values in the cells.");
   });
   $('button#record').click(function(){
     if (!validateFixedPoints()){
-      p("no fixed values!");
+      p("No fixed values!");
       return; 
     }
     var fixedPoints = getFixedPoints();
@@ -91,5 +101,9 @@ $(function() {
     $('button#clear').click();
     var fixedPoints = '{"0_0":5,"1_0":3,"0_1":6,"1_2":9,"2_2":8,"4_0":7,"3_1":1,"4_1":9,"5_1":5,"7_2":6,"0_3":8,"0_4":4,"0_5":7,"4_3":6,"3_4":8,"5_4":3,"4_5":2,"8_3":3,"8_4":1,"8_5":6,"1_6":6,"3_7":4,"4_7":1,"5_7":9,"4_8":8,"6_6":2,"7_6":8,"8_7":5,"7_8":7,"8_8":9}';
     displayPoints(JSON.parse(fixedPoints), fixClass);
+    refreshFixedPointsCount();
+    p("Added values in the cells as an example, then you just click the Computer button.");
   });
+  
+  refreshFixedPointsCount();
 });
